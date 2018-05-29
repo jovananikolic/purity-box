ddd93c893d8ddce98ec2ac55c8b174426dbe307d
Jovana Nikolić <jovana.nikolic@cubes.rs>
2018-05-29 19:27:07 +0200

    Sredjivanje headera (dropdown liste)

--- purity-box/public_html/js/main.js
+++ purity-box/public_html/js/main.js
@@ -1,6 +1,19 @@
 $(document).ready(function(){
     
-
-
+    $('.purity-dropdown .nav-link').click(function(e) {
+       e.preventDefault();
+       $('.purity-packages-dropdown').slideToggle();
+    });
     
+    $('.shop-dropdown .nav-link').click(function(e) {
+       e.preventDefault();
+       $('.shop-products-dropdown').slideToggle();
+    });
+
+
+    $(".show-search").click(function (e) {
+        e.preventDefault();
+        $(this).toggleClass('fa-search fa-close');
+        $(".search-form").toggleClass("open-search");
+    });
 });
